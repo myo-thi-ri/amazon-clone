62a778f156b1e56f135b4983653c27351cb38ab7
MYO THIRI <sandarlamin.llm@gmail.com>
2020-11-15 18:58:10 +0900

    Initial Commit.Complete the header component.

---
+++ amazon-clone/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div>
+            <h1>I am the Home component.</h1>
+        </div>
+    )
+}
+
+export default Home
---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCBCYDi8lOEdzTI4xsbvmWX4cktI8yWjZM",
+    authDomain: "challenge-72074.firebaseapp.com",
+    databaseURL: "https://challenge-72074.firebaseio.com",
+    projectId: "challenge-72074",
+    storageBucket: "challenge-72074.appspot.com",
+    messagingSenderId: "257548019873",
+    appId: "1:257548019873:web:4d5d85d123a78fcad2e8e0",
+    measurementId: "G-LGHY8MF9KC"
+  };

0c55344542b4e2ba95ef32ee274ba06be1ea1f00
MYO THIRI <sandarlamin.llm@gmail.com>
2020-11-18 21:36:57 +0900

    Complete user authentification and sign in/out features.

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -2,20 +2,34 @@ import React from 'react'
 import './Header.css'
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { Link } from 'react-router-dom';
+import { useStateValue } from './StateProvider';
+import { auth } from './firebase';
 
 function Header() {
+    const [{ basket, user }, dispatch] = useStateValue();
+
+    const handleAuthentication = () => {
+        if(user){
+            auth.signOut();
+        }
+    }
     return (
         <div className="header">
-            <img className="header__logo" src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'/>
+            <Link to="/">
+                <img className="header__logo" src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'/>
+            </Link>
             <div className='header__search'>
                 <input className="header__searchInput" type="text"/>
                 <SearchIcon className="header__searchIcon"/>
             </div>
             <div className="header__nav">
-                <div className='header__option'>
-                    <span className='header__optionLineOne'>Hello, Guest</span>
-                    <span className='header__optionLineTwo'>Sign in</span>
-                </div>
+                <Link to={!user && '/login'}>
+                    <div onClick={handleAuthentication} className='header__option'>
+                        <span className='header__optionLineOne'>Hello, Guest</span>
+    <span className='header__optionLineTwo'>{user ? 'Sign Out' : 'Sign In'}</span>
+                    </div>
+                </Link>
                 <div className='header__option'>
                     <span className='header__optionLineOne'>Returns</span>
                     <span className='header__optionLineTwo'>&Orders</span>
@@ -24,10 +38,14 @@ function Header() {
                     <span className='header__optionLineOne'>Your</span>
                     <span className='header__optionLineTwo'>Prime</span>
                 </div>
-                <div className='header__optionBasket'>
-                    <ShoppingBasketIcon />
-                    <span className='header__optionLineTwo header__basketCount'>0</span>
-                </div>
+                <Link to="/checkout">
+                    <div className='header__optionBasket'>
+                        <ShoppingBasketIcon />
+                        <span className='header__optionLineTwo header__basketCount'>
+                            {basket?.length}
+                        </span>
+                    </div>
+                </Link>
     
 
             </div>
--- amazon-clone/src/firebase.js
+++ amazon-clone/src/firebase.js
@@ -1,11 +1,21 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCBCYDi8lOEdzTI4xsbvmWX4cktI8yWjZM",
-    authDomain: "challenge-72074.firebaseapp.com",
-    databaseURL: "https://challenge-72074.firebaseio.com",
-    projectId: "challenge-72074",
-    storageBucket: "challenge-72074.appspot.com",
-    messagingSenderId: "257548019873",
-    appId: "1:257548019873:web:4d5d85d123a78fcad2e8e0",
-    measurementId: "G-LGHY8MF9KC"
-  };
+  apiKey: "AIzaSyCBCYDi8lOEdzTI4xsbvmWX4cktI8yWjZM",
+  authDomain: "challenge-72074.firebaseapp.com",
+  databaseURL: "https://challenge-72074.firebaseio.com",
+  projectId: "challenge-72074",
+  storageBucket: "challenge-72074.appspot.com",
+  messagingSenderId: "257548019873",
+  appId: "1:257548019873:web:4d5d85d123a78fcad2e8e0",
+  measurementId: "G-LGHY8MF9KC"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
